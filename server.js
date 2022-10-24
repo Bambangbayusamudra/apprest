df4caf8bf76eb98f12ae58e2e98b58d14e5a71af
bambang.wetalk <bambang.wetalk@gmail.com>
2022-10-24 14:02:17 +0700

    seting routes di server js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();   //fungsi secara global untuk memanggil express js
 //parse aplication/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
  app.listen(3000, () =>{
     console.log('server started on port'); 
  });
