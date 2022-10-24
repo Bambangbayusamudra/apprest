484b59f61682edbf2f00d53e5deafa50802e243c
bambang.wetalk <bambang.wetalk@gmail.com>
2022-10-24 14:47:51 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 'use strict';
 
 var response = require('./res');
-var connection = require('./konecksi');
+var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API Berjalan")
+    response.ok("Aplikasi REST API Berjalan", res)
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows,res)
+        }
+    });
 };
