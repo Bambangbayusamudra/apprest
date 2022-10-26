80b6406a654dd6b6c822de7cebe4664def0dafb2
bambang.wetalk <bambang.wetalk@gmail.com>
2022-10-26 15:49:35 +0700

    latest

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,29 +1,40 @@
 const jwt = require('jsonwebtoken');
+const connection = require('../koneksi');
+var mysql = require('mysql');
 const config = require('../config/secret');
 
 function verifikasi(){
     return function(req, rest, next){
-        var role = req.body.role;
+        // var role = req.body.role;
         // cek authorization header
-        var tokenWithBearer = req.headers.authorization;
-        if(tokenWithBearer){
-            var token = tokenWithBearer.split(' ')[1];
-            //veritfikasi
-            jwt.verify(token, config.secret, function(err,decoded){
-                if(err){
-                    return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
-                }else{
-                    if(role == 2){
-                        req.auth = decoded;
-                        next();
-                    }else{
-                        return rest.status(401).send({auth:false, message:'gagal mengotorisasi role anda!'});
-                    }
-                }
-            });
-        }else{
-            return rest.status(401).send({auth:false, message:'Token tidak tersedia!'});
+        var get = {
+            user_id: req.body.user_id,
+            access_token: req.body.access_token,
         }
+        var tokenWithBearer = req.headers.authorization;
+        var query = "SELECT ??=? FROM ?? WHERE ??=? ";
+        var table = ["user_id",get.user_id , "akses_token", "access_token", get.access_token];
+        query = mysql.format(query,table);
+        connection.query(query,function(rows){ 
+            if(tokenWithBearer){
+                var token = tokenWithBearer.split(' ')[1];
+                //veritfikasi
+                jwt.verify(token, config.secret, function(err,decoded){
+                    if(err){
+                        return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
+                    }else{
+                        if(query){
+                            req.auth = decoded;
+                            next();
+                        }else{
+                            return rest.status(401).send({auth:false, message:'gagal mengotorisasi role anda!'});
+                        }
+                    }
+                });
+            }else{
+                return rest.status(401).send({auth:false, message:'Token tidak tersedia!'});
+            }
+        });
     }
 }
 
